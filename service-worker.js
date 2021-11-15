bc2ae097bfafc5877b0f2e07f8a56908969d6a13
Fredrik Wallner <fredrik@wallner.nu>
2021-11-15 13:40:28 +0100

    Added version control and reload link

--- service-worker.js
+++ service-worker.js
@@ -42,8 +42,8 @@ self.addEventListener("fetch", (evt) => {
         fetch(evt.request).catch(() => {
             return caches.open(CACHE_NAME).then((cache) => {
                 let curl = evt.request.url.replace(/https?:\/\/[^/]*/, "");
-                if (curl === "/beteende/") {
-                    curl = "/beteende/index.html"
+                if (curl[curl.length - 1] === "/") {
+                    curl += "index.html"
                 }
                 return cache.match(curl);
             });
